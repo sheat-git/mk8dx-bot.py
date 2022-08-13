22c513dfe992afbea9bdf7afc42d0c82e80def0e
sheat <dev.sheat@gmail.com>
2022-08-13 18:34:22 +0900

    update

--- track/track_map_image.py
+++ track/track_map_image.py
@@ -17,11 +17,14 @@ def concat(im1, im2):
     dst.paste(_im2, (_im1.width, 0))
     return dst
 
-for i in range(14):
-    cup = Image.open(f'track/images/cups/{i}.jpg')
+i = 0
+while True:
+    url = f'https://raw.githubusercontent.com/sheat-git/mk8dx/main/cups/{i}.jpg'
+    cup = Image.open(io.BytesIO(requests.get(url).content))
     _i = {4: 8, 5: 10, 6: 4, 7: 5, 8: 6, 9: 7, 10: 9}.get(i, i)
     for j in range(4):
         id = _i*4 + j + 1
         url = f'http://japan-mk.blog.jp/mk8dx.info-4/table/{id:02}.jpg'
         map = Image.open(io.BytesIO(requests.get(url).content))
         concat(cup, map).save(f'track/images/tracks/{i*4+j}.jpg')
+    i += 1
