b5d7052de956b155bb42db565e51fce38d5c3d52
sheat <dev.sheat@gmail.com>
2023-02-05 14:35:43 +0900

    update track images

--- track/cog.py
+++ track/cog.py
@@ -51,7 +51,7 @@ class TrackCog(commands.Cog, name='Track'):
             title=f'{track.abbr} {track.abbr_ja}',
             description=f'{track.full_name}\n{track.full_name_ja}'
         )
-        if track.id < 64:
+        if track.id < 72:
             embed.set_image(url=f'https://raw.githubusercontent.com/sheat-git/mk8dx/main/tracks/20230205/{track.id}.jpg')
         else:
             embed.set_image(url=f'https://raw.githubusercontent.com/sheat-git/mk8dx/main/cups/{track.cup.id}.jpg')
